ea0595427f0b1aa0f9f43e2deb04ff773d7df0b4
dongphusang <marcodsang@gmail.com>
2023-02-11 21:33:18 -0500

    Initial

---
+++ Terra/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Terra;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Terra/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Terra;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
