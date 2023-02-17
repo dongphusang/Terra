2b80c11e10309b581df78c20d92eef22e8771f71
dongphusang <marcodsang@gmail.com>
2023-02-17 15:36:05 -0500

    AddWorkspacePage UI
    - Added UI for adding workspaces

--- Terra/MauiProgram.cs
+++ Terra/MauiProgram.cs
@@ -9,12 +9,15 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.UseMauiApp<App>()
+			.UseMauiApp<App>()			
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("LDFComicSans.tff", "LDFComicSans");
+				fonts.AddFont("LDFComicSansBold.tff", "LDFComicSansBold");
+
+            });
 
 		builder.Services.AddSingleton<MainPage>();
 		builder.Services.AddSingleton<MainPageViewModel>();
